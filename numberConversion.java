2f1f335c927a0630aba83309ed900a6d8bed054f
pulok <rifatahsan.pulok11@gmail.com>
2023-09-26 23:59:34 +0600

    Added NumberConversion Java program for decimal to binary, octal, and hexadecimal conversions
    This commit adds a Java program that allows users to input a decimal number and select a conversion type: binary, octal, or hexadecimal. The program performs the conversion and displays the result. It also handles fractional parts and displays the result accordingly.
    
    - Created a NumberConversion class with methods for hexadecimal conversion, array to hexadecimal conversion, and decimal conversion.
    - Implemented decimal to binary, octal, and hexadecimal conversion functionality.
    - Validated user input for the conversion type (1 for binary, 2 for octal, 3 for hexadecimal).
    - Displayed the converted result.
    - Added input prompts and error handling for invalid inputs.
    
    Usage:
    1. Enter a decimal number.
    2. Select the conversion type (1 for binary, 2 for octal, 3 for hexadecimal).
    3. The program displays the converted result.
    
    This program provides a useful utility for converting decimal numbers into different number systems.
    
    [Optional: Add more details or explanations if needed]

--- numberConversion.java
+++ numberConversion.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class numberConversion {
+public class numberConversion {
+    public static int[] converison(double number, int selectnumber){
+        int div;
+        switch (selectnumber){
+            case 1:
+                div = 2;
+                break;
+            case 2:
+                div = 8;
+                break;
+            case 3;
+                div = 16;
+        }
+        int duplicate = (int) number,index = 0,sum[];
+        int i = duplicate;
+        while (i>=0){
+            if (i==0){
+                sum[index] = 0;
+            }
+            sum[index] = (i%div)
+            i/=div;
+            counter++;
+
+        }
+        for (int i = 0 ; i <=index/2;i++){
+            int k = sum[index];
+            sum[index] = sum[index-i];
+            sum[index-i] = k;
+        }
+        sum[index++] = .;
+        index++;
+        //to separate the fractional number
+        int fractionalNumber = number - duplicate;
+        int count = 0;
+        while (count<=3){
+            if (sum[index] == 0){
+                count++;
+            }
+            sum[index] = fractionalNumber*div;
+
+        }
+    }
 }
--- src/NumberConversion.java
+++ src/NumberConversion.java
@@ -1,8 +1,29 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class NumberConversion {
-    public static int[] conversion(double number, int selectNumber) {
+class NumberConversion {
+    public static String Hexadecimal(int number) {
+        if (number <= 9) {
+            return Integer.toString(number);
+            //type casting the integer to character then applying the logic to find the hexadecimal value;
+        } else {
+            return Character.toString((char) ('A' + number - 10));
+        }
+    }
+
+    public static ArrayList<String> ArrayToHexadecimal(ArrayList<Integer> array) {
+        ArrayList<String> HexArray = new ArrayList<>();
+        for (int num : array) {
+            HexArray.add(Hexadecimal(num));
+        }
+        return HexArray;
+    }
+
+    public static ArrayList<Integer> conversion(double number, int selectNumber) {
+        int real = (int) number;
+        double fractional = number - real;
+        ArrayList<Integer> result = new ArrayList<>();
+
         int div;
         switch (selectNumber) {
             case 1:
@@ -15,50 +36,61 @@ public class NumberConversion {
                 div = 16;
                 break;
             default:
-                div = 2; // Default to binary if an invalid option is chosen
+                System.out.println("Enter between 1 to 3");
+                return result; // Return an empty list if the input is invalid.
         }
 
-        int duplicate = (int) number;
-        int i = duplicate;
-        int index = 0;
-        int[] sum = new int[100]; // Initialize the sum array with a size
-
-        while (i > 0) {
-            sum[index] = i % div;
-            i /= div;
-            index++;
+        while (real > 0) {
+            result.add(0, real % div);
+            real = real / div;
         }
-        // Reverse the array
-        for (i = 0; i < index / 2; i++) {
-            int temp = sum[i];
-            sum[i] = sum[index - i - 1];
-            sum[index - i - 1] = temp;
+        if (number>(int)number) {
+            result.add(-1); // Add the decimal point indicator
+            int count = 0;
+            while (count <= 4) {
+                double newNum = fractional * div;
+                if (newNum > 1) {
+                    int newNum2 = (int) newNum;
+                    result.add(newNum2);
+                    fractional = newNum - newNum2;
+                } else {
+                    result.add(0);
+                    fractional = newNum;
+                    count++;
+                }
+            }
         }
-
-        index++;
-        // Separate the fractional number
-        double fractionalNumber = number - duplicate;
-        int count = 0;
-        while (count <= 3) {
-            fractionalNumber *= div;
-            sum[index] = (int) fractionalNumber;
-            fractionalNumber -= sum[index];
-            index++;
-            count++;
-        }
-
-        return sum;
+        return result;
     }
+
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a Decimal Number to Convert: ");
+        System.out.println("Enter a number: ");
         double number = input.nextDouble();
-        System.out.println("Press any number: \n1. Decimal to Binary\n2. Decimal to Octal\n3. Decimal to Hexadecimal");
+        System.out.println("1. Decimal to Binary\n2. Decimal to Octal\n3. Decimal to Hexadecimal\nSelect one from above.");
         int selectNumber = input.nextInt();
-        int[] numbers = conversion(number, selectNumber);
-
-        // Print the result
-        System.out.println("Converted number: " + Arrays.toString(numbers));
+        ArrayList<Integer> result = NumberConversion.conversion(number, selectNumber);
+        ArrayList<String> result1 = NumberConversion.ArrayToHexadecimal(result);
+        if (selectNumber == 1 || selectNumber == 2) {
+            if (!result.isEmpty()) {
+                System.out.print("Result: ");
+                for (int num : result) {
+                    if (num == -1) {
+                        System.out.print(".");
+                    } else {
+                        System.out.print(num);
+                    }
+                }
+                System.out.println();
+            }
+        } else if (selectNumber == 3) {
+               StringBuilder result2 = new StringBuilder();
+               for (String str : result1){
+                   result2.append(str);
+               }
+               String finalResult = result2.toString();
+            System.out.println(finalResult);
+        }
     }
 }
