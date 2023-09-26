d62cd19eced6506ac2298184aab2c04635d96995
pulok <rifatahsan.pulok11@gmail.com>
2023-09-27 00:13:57 +0600

    Added NumberConversion Java program for decimal to binary, octal, and hexadecimal conversions
    This commit adds a Java program that allows users to input a decimal number and select a conversion type: binary, octal, or hexadecimal. The program performs the conversion and displays the result. It also handles fractional parts and displays the result accordingly.
    
    - Created a NumberConversion class with methods for hexadecimal conversion, array to hexadecimal conversion, and decimal conversion.
    - Implemented decimal to binary, octal, and hexadecimal conversion functionality.
    - Validated user input for the conversion type (1 for binary, 2 for octal, 3 for hexadecimal).
    - Displayed the converted result.
    - Added input prompts and error handling for invalid inputs.
    
    Usage:
    1. Enter a decimal number.
    2. Select the conversion type (1 for binary, 2 for octal, 3 for hexadecimal).
    3. The program displays the converted result.
    
    This program provides a useful utility for converting decimal numbers into different number systems.
    
    [Optional: Add more details or explanations if needed]

--- src/NumberConversion.java
+++ src/NumberConversion.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class NumberConversion {
+    // Convert a decimal number to a hexadecimal digit
     public static String Hexadecimal(int number) {
         if (number <= 9) {
-            return Integer.toString(number);
-            //type casting the integer to character then applying the logic to find the hexadecimal value;
+            return Integer.toString(number); // If it's a digit 0-9, return the number as a string
         } else {
+            // If it's 10 or greater, convert it to A-F using character mapping
             return Character.toString((char) ('A' + number - 10));
         }
     }
 
+    // Convert an ArrayList of integers to hexadecimal digits
     public static ArrayList<String> ArrayToHexadecimal(ArrayList<Integer> array) {
         ArrayList<String> HexArray = new ArrayList<>();
         for (int num : array) {
@@ -19,35 +21,39 @@ class NumberConversion {
         return HexArray;
     }
 
+    // Perform decimal to binary, octal, or hexadecimal conversion
     public static ArrayList<Integer> conversion(double number, int selectNumber) {
-        int real = (int) number;
-        double fractional = number - real;
+        int real = (int) number; // Extract the integer part
+        double fractional = number - real; // Extract the fractional part
         ArrayList<Integer> result = new ArrayList<>();
 
         int div;
         switch (selectNumber) {
             case 1:
-                div = 2;
+                div = 2; // Binary conversion
                 break;
             case 2:
-                div = 8;
+                div = 8; // Octal conversion
                 break;
             case 3:
-                div = 16;
+                div = 16; // Hexadecimal conversion
                 break;
             default:
                 System.out.println("Enter between 1 to 3");
                 return result; // Return an empty list if the input is invalid.
         }
 
+        // Integer part conversion
         while (real > 0) {
             result.add(0, real % div);
             real = real / div;
         }
-        if (number>(int)number) {
+
+        // Fractional part conversion
+        if (number > (int) number) {
             result.add(-1); // Add the decimal point indicator
             int count = 0;
-            while (count <= 4) {
+            while (count <= 4) { // Convert up to 4 decimal places
                 double newNum = fractional * div;
                 if (newNum > 1) {
                     int newNum2 = (int) newNum;
@@ -62,7 +68,6 @@ class NumberConversion {
         }
         return result;
     }
-
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -85,11 +90,11 @@ class NumberConversion {
                 System.out.println();
             }
         } else if (selectNumber == 3) {
-               StringBuilder result2 = new StringBuilder();
-               for (String str : result1){
-                   result2.append(str);
-               }
-               String finalResult = result2.toString();
+            StringBuilder result2 = new StringBuilder();
+            for (String str : result1) {
+                result2.append(str);
+            }
+            String finalResult = result2.toString();
             System.out.println(finalResult);
         }
     }
